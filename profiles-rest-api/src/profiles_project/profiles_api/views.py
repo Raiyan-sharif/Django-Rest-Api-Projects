96572d6508ba5988c261ed958b92086d4cf940b9
Raiyan <raiyan.sharif.1234@gmail.com>
2019-05-05 16:48:02 +0600

    search function added

--- profiles-rest-api/src/profiles_project/profiles_api/views.py
+++ profiles-rest-api/src/profiles_project/profiles_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import  APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from .permissions import *
 from .models import UserProfile
@@ -97,3 +97,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
